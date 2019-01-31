b7043eaed3efeb4e32c9b50ee9f01764bbba1c22
rasoulian <arasoulian@gmail.com>
2019-01-31 14:20:33 +0330

    add idea

---
+++ LicenseUtils.Core/ILicense.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+
+namespace LicenseUtils.Core
+{
+    public interface ILicense
+    {
+        bool IsLicenseValid();
+
+        void GetLicenseInfo();
+    }
+
+    public class License : ILicense
+    {
+        public bool IsLicenseValid()
+        {
+            Console.WriteLine("License has been valid!");
+            return true;
+        }
+
+        public void GetLicenseInfo()
+        {
+            Console.WriteLine("License has been valid!");
+        }
+    }
+
+    public class Edition
+    {
+        public int DeviceCount { get; set; }
+        public int EmployeeCount { get; set; }
+
+        public Edition(int deviceCount, int employeeCount)
+        {
+            this.DeviceCount = deviceCount;
+            this.EmployeeCount = employeeCount;
+        }
+    }
+
+    public class LicenseProxy : ILicense
+    {
+        private Edition edition;
+        private ILicense license;
+
+        public LicenseProxy(Edition edition)
+        {
+            this.edition = edition;
+            this.license = new License();
+        }
+
+        public bool IsLicenseValid()
+        {
+            return license.IsLicenseValid();
+        }
+
+        public void GetLicenseInfo()
+        {
+            
+        }
+    }
+
+}
